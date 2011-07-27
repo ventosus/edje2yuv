5865ea39b88eb07549ecedac284caaf3341e2e0a
Hanspeter Portner <none@none>
2011-07-27 21:10:45 +0200

    added more types of yuv4mpeg codecs, added raw rgba output, got rid of scale parameter

--- Makefile
+++ Makefile
@@ -2,12 +2,15 @@ ECORE_OPTS = `pkg-config --cflags --libs ecore`
 ECORE_EVAS_OPTS = `pkg-config --cflags --libs ecore-evas`
 EVAS_OPTS = `pkg-config --cflags --libs evas`
 EDJE_OPTS = `pkg-config --cflags --libs edje`
-PNG_OPTS = `pkg-config --cflags --libs libpng`
 YUV_OPTS = `pkg-config --cflags --libs mjpegtools`
 SW_OPTS = `pkg-config --cflags --libs libswscale`
 OPTS = $(ECORE_OPTS) $(ECORE_EVAS_OPTS) $(EVAS_OPTS) $(EDJE_OPTS) $(PNG_OPTS) $(YUV_OPTS) $(SW_OPTS)
 
-all:	edje2yuv
+WIDTH=800
+HEIGHT=200
+FPS=60
+
+all:	edje2yuv sample.edj
 
 edje2yuv:	edje2yuv.c
 	gcc -g -o $@ $< $(OPTS)
@@ -15,11 +18,26 @@ edje2yuv:	edje2yuv.c
 sample.edj:	sample.edc
 	edje_cc -id cc $< $@
 
-sample.mkv:	edje2yuv sample.edj
-	./edje2yuv -i sample.edj -w 800 -h 200 -f 25 | ffmpeg -y -f yuv4mpegpipe -i - -b 5000000 sample.mkv
+sample.y4m:	edje2yuv sample.edj
+	./edje2yuv -i sample.edj -w $(WIDTH) -h $(HEIGHT) -f $(FPS) -y mono -o sample.y4m
 
-test:	sample.mkv
+sample.mkv:	edje2yuv sample.edj
+	./edje2yuv -i sample.edj -w $(WIDTH) -h $(HEIGHT) -f $(FPS) -y 420mpeg2 | ffmpeg -y -f yuv4mpegpipe -i - -b 5000000 sample.mkv
+
+sample.pipe:	edje2yuv sample.edj
+	./edje2yuv -i sample.edj -w $(WIDTH) -h $(HEIGHT) -f $(FPS) -y 411 | ffplay -autoexit -
+
+sample.raw.pipe:	edje2yuv sample.edj
+	./edje2yuv -i sample.edj -w $(WIDTH) -h $(HEIGHT) -f $(FPS) -r | \
+	  ffmpeg -y -s $(WIDTH)x$(HEIGHT) -r $(FPS) -f rawvideo -vcodec rawvideo -pix_fmt rgba -i - -f yuv4mpegpipe -pix_fmt yuv420p - | \
+	  ffplay -autoexit -
+
+sample.raw:	edje2yuv sample.edj
+	./edje2yuv -i sample.edj -w $(WIDTH) -h $(HEIGHT) -f $(FPS) -o sample.raw -r
+
+sample:	edje2yuv sample.edj
+	./edje2yuv -i sample.edj -w $(WIDTH) -h $(HEIGHT) -f $(FPS) -t
 
 clean:
-	rm -f edje2yuv sample.edj sample.mkv
+	rm -f edje2yuv sample.edj sample.mkv sample.y4m sample.raw
 
--- edje2yuv.c
+++ edje2yuv.c
@@ -4,7 +4,7 @@
  *  further integration into films or presentations.
  *
  *
- *  Copyright (c) 2010 Hanspeter Portner <ventosus@airpost.net>
+ *  Copyright (c) 2010-2011 Hanspeter Portner <ventosus@airpost.net>
  *
  *
  *  This program is free software; you can redistribute it and/or
@@ -40,7 +40,7 @@
 Ecore_Evas *ee = NULL;
 Evas *evas = NULL;
 Evas_Object *edj = NULL;
-Ecore_Animator *ea = NULL;
+Ecore_Timer *ea = NULL;
 
 // command line arguments
 int test = 0;
@@ -51,6 +51,8 @@ char *out = "-";
 char PATH[256];
 int width = 0;
 int height = 0;
+int chroma = Y4M_CHROMA_420MPEG2;
+int raw = 0;
 
 // yuv4mpeg
 FILE *stream = NULL;
@@ -58,11 +60,11 @@ y4m_stream_info_t si;
 y4m_frame_info_t fi;
 
 // swscale stuff
-struct SwsContext *srcContext, *dstContext = NULL;
+struct SwsContext *dstContext = NULL;
 int srcFormat = PIX_FMT_BGRA;
 int dstFormat = PIX_FMT_YUV420P;
-int srcStride [] = {0, 0, 0};
-int dstStride [] = {0, 0, 0};
+int srcStride [] = {0, 0, 0, 0};
+int dstStride [] = {0, 0, 0, 0};
 uint8_t *planar420 [3];
 
 void
@@ -72,6 +74,7 @@ init ()
    ecore_evas_init ();
    evas_init ();
    edje_init ();
+   y4m_accept_extensions (1);
 }
 
 void
@@ -94,17 +97,31 @@ abort_ (const char *s, ...)
    abort ();
 }
 
-int
+Eina_Bool
 dump (void *udata)
 {
-  int stream_nr = fileno (udata);
-  const uint8_t * buf = ecore_evas_buffer_pixels_get (ee);
-  const uint8_t *src [3] = {buf, NULL, NULL};
+  //edje_object_calc_force (edj);
+  edje_object_play_set (edj, EINA_FALSE);
+  ecore_timer_freeze (ea);
 
-  sws_scale(dstContext, src, srcStride, 0, height, planar420, dstStride);
-  y4m_write_frame (stream_nr, &si, &fi, planar420);
+  ecore_main_loop_iterate ();
 
-  return 1;
+  FILE *f = udata;
+  int stream_nr = fileno (f);
+  const uint8_t *buf = ecore_evas_buffer_pixels_get (ee);
+
+  if (raw)
+    fwrite (buf, sizeof (uint8_t), width*height*4, f);
+  else
+  {
+    sws_scale (dstContext, &buf, srcStride, 0, height, planar420, dstStride);
+    y4m_write_frame (stream_nr, &si, &fi, planar420);
+  }
+
+  ecore_timer_thaw (ea);
+  edje_object_play_set (edj, EINA_TRUE);
+
+  return EINA_TRUE;
 }
 
 void
@@ -118,7 +135,7 @@ int
 main (int argc, char **argv)
 {
    int c;
-   while ( (c = getopt (argc, argv, "i:o:w:h:f:s:t")) != -1)
+   while ( (c = getopt (argc, argv, "i:o:w:h:f:y:rt")) != -1)
      switch (c)
        {
 	case 'i':
@@ -133,17 +150,23 @@ main (int argc, char **argv)
 	case 'h':
 	   height = atoi (optarg);
 	   break;
-	case 't':
-	   test = 1;
-	   break;
 	case 'f':
 	   fps = atoi (optarg);
 	   break;
-	case 's':
-	   scale = atoi (optarg);
+	case 'y':
+	   chroma = y4m_chroma_parse_keyword (optarg);
+	   fprintf (stderr, "chroma: %s\n", y4m_chroma_description (chroma));
+	   if (chroma == Y4M_UNKNOWN)
+	     chroma = Y4M_CHROMA_420MPEG2;
+	   break;
+	case 't':
+	   test = 1;
+	   break;
+	case 'r':
+	   raw = 1;
 	   break;
 	case '?':
-	   if ( (optopt == 'i') || (optopt == 'o') || (optopt == 'w') || (optopt == 'h') || (optopt == 'f'))
+	   if ( (optopt == 'i') || (optopt == 'o') || (optopt == 'w') || (optopt == 'h') || (optopt == 'f') || (optopt == 'y'))
 	     fprintf (stderr, "Option `-%c' requires an argument.\n", optopt);
 	   else if (isprint (optopt))
 	     fprintf (stderr, "Unknown option `-%c'.\n", optopt);
@@ -156,9 +179,12 @@ main (int argc, char **argv)
 
    if (!in || !width || !height)
      {
-	fprintf (stderr, "usage: %s -i INPUT -w WIDTH -h HEIGHT [-o OUTPUT] [-t] [-f FPS] [-s SCALE]\n\n", argv[0]);
+	fprintf (stderr, "usage: %s -i INPUT -w WIDTH -h HEIGHT [-o OUTPUT] [-t] [-f FPS] [-y 420jpeg|420mpeg2|420paldv|444|422|411|mono|444alpha] [-r]\n\n", argv[0]);
 	abort ();
      }
+
+   fprintf (stderr, "width: %i\nheight: %i\nfps: %i\nraw: %i\nyuv: %s:\nin: %s\nout: %s\ntest: %i\n\n",
+       width, height, fps, raw, y4m_chroma_description (chroma), in, out, test);
    
    init ();
   
@@ -173,9 +199,10 @@ main (int argc, char **argv)
    edje_object_file_set (edj, in, "main");
    evas_object_resize (edj, width, height);
    evas_object_show (edj);
-   
+
    edje_object_signal_emit (edj, "start", "");
    edje_object_signal_callback_add (edj, "stop", "*", stop, NULL);
+   edje_frametime_set (1.0 / (double) fps);
   
    if (!test)
    {
@@ -186,66 +213,111 @@ main (int argc, char **argv)
      int stream_nr = fileno (stream);
 
      // efl
-     ea = ecore_animator_add (dump, stream);
-     edje_frametime_set (1.0 / (double) fps);
-     ecore_animator_frametime_set (1.0 / (double) fps);
+     ea = ecore_timer_add (1.0 / (double) fps, dump, stream);
 
-     // yuv4mpeg
-     y4m_ratio_t rt = {fps, 1};
-
-     y4m_init_stream_info (&si);
-     y4m_si_set_width (&si, width);
-     y4m_si_set_height (&si, height);
-     y4m_si_set_interlace (&si, Y4M_ILACE_NONE);
-     y4m_si_set_framerate (&si, rt);
-     y4m_si_set_chroma (&si, Y4M_CHROMA_420MPEG2);
-     y4m_write_stream_header (stream_nr, &si);
-
-     y4m_init_frame_info (&fi);
-     y4m_fi_set_presentation (&fi, Y4M_PRESENT_PROG_SINGLE);
-     y4m_fi_set_spatial (&fi, Y4M_SAMPLING_PROGRESSIVE);
-     y4m_fi_set_temporal (&fi, Y4M_SAMPLING_PROGRESSIVE);
-    
-     // swscale
-     srcStride[0] = 4*width;
-     dstStride[0] = width;
-     dstStride[1] = width/2;
-     dstStride[2] = width/2;
-     dstContext= sws_getContext (width, height, srcFormat, width, height, dstFormat, SWS_GAUSS, NULL, NULL, NULL);
-     if (!dstContext)
+     if (!raw)
      {
-       fprintf(stderr, "Failed to get %s ---> %s\n",
-	   sws_format_name(srcFormat),
-	   sws_format_name(dstFormat));
-       exit (-1);
+       // yuv4mpeg
+       y4m_ratio_t rt = {fps, 1};
+
+       y4m_init_stream_info (&si);
+       y4m_si_set_width (&si, width);
+       y4m_si_set_height (&si, height);
+       y4m_si_set_interlace (&si, Y4M_ILACE_NONE);
+       y4m_si_set_framerate (&si, rt);
+       y4m_si_set_chroma (&si, chroma);
+       y4m_write_stream_header (stream_nr, &si);
+
+       y4m_init_frame_info (&fi);
+       y4m_fi_set_presentation (&fi, Y4M_PRESENT_PROG_SINGLE);
+       y4m_fi_set_spatial (&fi, Y4M_SAMPLING_PROGRESSIVE);
+       y4m_fi_set_temporal (&fi, Y4M_SAMPLING_PROGRESSIVE);
+      
+       // swscale
+       srcStride[0] = 4*width;
+
+       switch (chroma)
+       {
+	 case Y4M_CHROMA_420JPEG:
+	   dstFormat = PIX_FMT_YUVJ420P;
+	   dstStride[0] = width;
+	   dstStride[1] = width/2;
+	   dstStride[2] = width/2;
+	   break;
+	 case Y4M_CHROMA_420PALDV:
+	   //TODO
+	 case Y4M_CHROMA_420MPEG2:
+	   dstFormat = PIX_FMT_YUV420P;
+	   dstStride[0] = width;
+	   dstStride[1] = width/2;
+	   dstStride[2] = width/2;
+	   break;
+	 case Y4M_CHROMA_444:
+	   dstFormat = PIX_FMT_YUV444P;
+	   dstStride[0] = width;
+	   dstStride[1] = width;
+	   dstStride[2] = width;
+	   break;
+	 case Y4M_CHROMA_422:
+	   dstFormat = PIX_FMT_YUV422P;
+	   dstStride[0] = width;
+	   dstStride[1] = width/2;
+	   dstStride[2] = width/2;
+	   break;
+	 case Y4M_CHROMA_411:
+	   dstFormat = PIX_FMT_YUV411P;
+	   dstStride[0] = width;
+	   dstStride[1] = width/4;
+	   dstStride[2] = width/4;
+	   break;
+	 case Y4M_CHROMA_MONO:
+	   dstFormat = PIX_FMT_GRAY8;
+	   dstStride[0] = width;
+	   break;
+	 case Y4M_CHROMA_444ALPHA:
+	   //TODO
+	   dstFormat = PIX_FMT_YUV444P;
+	   dstStride[0] = width;
+	   dstStride[1] = width;
+	   dstStride[2] = width;
+	   dstStride[3] = width;
+	   break;
+       }
+
+       dstContext = sws_getCachedContext (dstContext, width, height, srcFormat, width, height, dstFormat, SWS_FAST_BILINEAR, NULL, NULL, NULL);
+       if (!dstContext)
+       {
+	 fprintf(stderr, "Failed to get %s ---> %s\n",
+	     sws_format_name(srcFormat),
+	     sws_format_name(dstFormat));
+	 exit (-1);
+       }
+       
+       uint8_t *data = malloc (3*width*height);
+       planar420[0] = data;
+       planar420[1] = data + width*height;
+       planar420[2] = data + width*height*2;
      }
-     
-     uint8_t *data = malloc (3*width*height);
-     planar420[0] = data;
-     planar420[1] = data + width*height;
-     planar420[2] = data + width*height*2;
-   }
-   else
-   {
-     edje_frametime_set (1.0 / (double) fps);
-     ecore_animator_frametime_set ((double) scale / (double) fps);
    }
    
    ecore_main_loop_begin ();
   
    if (!test)
    {
-     free (planar420[0]);
-
-     y4m_fini_stream_info (&si);
-     y4m_fini_frame_info (&fi);
-
      if (strcmp (out, "-"))
        fclose (stream);
+
+     if (!raw)
+     {
+       free (planar420[0]);
+
+       y4m_fini_stream_info (&si);
+       y4m_fini_frame_info (&fi);
+     }
    }
   
    if (ea)
-     ecore_animator_del (ea);
+     ecore_timer_del (ea);
    evas_object_del (edj);
    evas_free (evas);
    ecore_evas_free (ee);
