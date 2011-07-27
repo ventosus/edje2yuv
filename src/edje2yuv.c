4887c42a7f2e8f5a0f43f00cb4edcd17023f1b2a
Hanspeter Portner <none@none>
2011-07-27 22:18:12 +0200

    added GNU root files

--- src/edje2yuv.c
+++ src/edje2yuv.c
@@ -1,27 +1,3 @@
-/*
- *  edje2yuv.c: A program to export Edje programs (layout/animation library
- *  of the Enlightenment foundation libraries) to a yuv4mpeg stream for
- *  further integration into films or presentations.
- *
- *
- *  Copyright (c) 2010-2011 Hanspeter Portner <ventosus@airpost.net>
- *
- *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
-
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
